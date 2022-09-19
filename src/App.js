6bbe2d6c009020bbb3d4aac1fea78c333cea40f7
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-19 11:13:21 -0400

    renders website visitors

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Sidebar from "./components/sidebar";
 import Reviews from "./components/reviews";
 import Rating from "./components/average-rating";
 import Sentiment from "./components/sentiment-analysis";
+import Visitors from "./components/web-visitors";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Reviews/>
       <Rating/>
       <Sentiment/>
+      <Visitors/>
     </div>
   );
 };
---
+++ src/components/web-visitors.js
@@ -0,0 +1,10 @@
+function Visitors(){
+    return(
+        <div>
+            <h4>Website Visitors</h4>
+            <h2>821</h2>
+        </div>
+    );
+};
+
+export default Visitors;
