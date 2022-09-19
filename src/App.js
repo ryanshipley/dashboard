1a01c3643f94143bb8d3e3a7ba3f2bb9e330ad8d
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-19 11:00:29 -0400

    renders reviews

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Sidebar from "./components/sidebar";
+import Reviews from "./components/reviews";
 
 function App() {
   return (
     <div>
       <Sidebar/>
+      <Reviews/>
     </div>
   );
 }
---
+++ src/components/reviews.js
@@ -0,0 +1,9 @@
+function Reviews(){
+    return(
+        <div>
+            1,281
+        </div>
+    );
+};
+
+export default Reviews;
