4a48311c014718562d9e9b24454a0fc9fb987314
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-19 23:36:31 -0400

    adds box in visitors

--- src/components/web-visitors.js
+++ src/components/web-visitors.js
@@ -3,6 +3,7 @@ function Visitors(){
         <div className="visitors">
             <h4>Website Visitors</h4>
             <h2>821</h2>
+            <div className="box"></div>
         </div>
     );
 };
